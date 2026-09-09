1c37e9dad1d7b90b04b64b0ab5f57ce0f27181f1
Junaid Abbasi <juni.abbasi@icloud.com>
2026-09-09 03:18:51 -0500

    All 21 test passed

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+function calculateTax(amount) {
+    const taxAmount = amount * 0.1
+    return taxAmount
+}
+function convertToUpperCase(text) {
+    return text.toUpperCase()
+}
+function findMaximum(num1, num2) {
+    const maxValue = Math.max(num1, num2)
+    return maxValue
+}
+function isPalindrome(text) {
+    const reversedWord = text.split("").reverse().join("")
+    return text === reversedWord
+}
 
-
+function calculateDiscountedPrice(originalPrice, discountedPercentage) {
+    const discountedAmount = originalPrice * (discountedPercentage / 100)
+    const discountPrice = originalPrice - discountedAmount
+    return discountPrice
+}
 
 
 
